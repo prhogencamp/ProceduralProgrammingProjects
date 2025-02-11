dc4e2c5f16e45997d138f3aced40d0e2b49f30e9
Patrick <ph68040@email.vccs.edu>
2025-02-11 13:34:42 -0500

    Copied Rock Paper Scissors project from my original file

--- ProceduralProgrammingProjects/ProceduralProgrammingProjects.cpp
+++ ProceduralProgrammingProjects/ProceduralProgrammingProjects.cpp
@@ -2,19 +2,147 @@
 //
 
 #include <iostream>
+#include <random>
+using namespace std;
+
+int getPlayerChoice();
+int getComputerChoice();
+int chooseWinner(int player, int computer);
+void showChoice(int choice);
+void declareWinner(int playerScore, int computerScore);
+void updateScores(char winner, int& playerScore, int& computerScore);
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int playerScore = 0;
+    int computerScore = 0;
+    int playerChoice;
+    int computerChoice;
+    char winner;
+
+    while (true) {
+        playerChoice = getPlayerChoice();
+        if (playerChoice == 4) {
+            break;
+        }
+        cout << "Your Choice: " << endl;
+        showChoice(playerChoice);
+
+        computerChoice = getComputerChoice();
+        cout << "Computer's Choice: " << endl;
+        showChoice(computerChoice);
+
+        winner = chooseWinner(playerChoice, computerChoice);
+        updateScores(winner, playerScore, computerScore);
+    }
+    declareWinner(playerScore, computerScore);
+
+    return 0;
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
+int getPlayerChoice() {
+    int player;
+    while (true) {
+        cout << "Make your choice:\n";
+        cout << "1: Rock" << endl;
+        cout << "2: Paper" << endl;
+        cout << "3: Scissors" << endl;
+        cout << "4: Quit" << endl;
+        cin >> player;
+        if (cin.fail() || player < 1 || player > 4) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            cout << "That is not a valid input. Try again." << endl;
+        }
+
+        else {
+            break;
+        }
+
+    }
+    return player;
+}
+int getComputerChoice() {
+
+    random_device engine;
+    uniform_int_distribution<int>computerThrow(1, 3);
+
+    // switch (computerThrow(engine)) {
+   //  case 1: return 1;
+    // case 2: return 2;
+    // case 3: return 3;
+     //}
+
+    return computerThrow(engine);
+
+}
+
+
+
+
+
+int chooseWinner(int player, int computer) {
+    if (player == computer) {
+        return 'd'; //Draw
+    }
+    else if ((player == 1 && computer == 3) ||
+        (player == 2 && computer == 1) ||
+        (player == 3 && computer == 2)) {
+        return 'p'; // Player win
+
+    }
+    else {
+        return 'c'; // Computer win
+    }
+
+    return 0;
+}
+
+void updateScores(char winner, int& playerScore, int& computerScore) {
+    if (winner == 'p') {
+        cout << "You win this round!" << endl;
+        playerScore++;
+    }
+    else if (winner == 'c') {
+        cout << "Computer wins this round!" << endl;
+        computerScore++;
+    }
+    else {
+        cout << "It's a draw!" << endl;
+    }
+    cout << "Scores are Player: " << playerScore << ", Computer: " << computerScore << endl;
+}
+
+void declareWinner(int playerScore, int computerScore) {
+    cout << "Final scores are Player: " << playerScore << ", Computer: " << computerScore << endl;
+    if (playerScore > computerScore) {
+        cout << "Congratulations! You've won the tournament!" << endl;
+    }
+    else if (computerScore > playerScore) {
+        cout << "The computer has won the tournament. Better luck in the next tournament." << endl;
+    }
+    else {
+        cout << "The tournament has ended in a tie!" << endl;
+    }
+}
+
+void showChoice(int choice) {
+
+    switch (choice) {
+    case 1: cout << "Rock" << endl;
+        break;
+    case 2: cout << "Paper" << endl;
+        break;
+    case 3: cout << "Scissors" << endl;
+        break;
+    }
+}
+
